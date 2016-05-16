65c6c18b785680c030b97a1248a2feae609c7385
Timothy Day <the.tday@gmail.com>
2016-05-16 14:55:43 -0400

    Added my agent

---
+++ src/ch/idsia/agents/BehaviorMarioAgent.java
@@ -0,0 +1,50 @@
+package ch.idsia.agents;
+
+import ch.idsia.benchmark.mario.environments.Environment;
+
+public class BehaviorMarioAgent implements Agent {
+
+	@Override
+	public boolean[] getAction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void integrateObservation(Environment environment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void giveIntermediateReward(float intermediateReward) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setObservationDetails(int rfWidth, int rfHeight, int egoRow,
+			int egoCol) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
